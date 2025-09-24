b2c29fd0b148e73adde9200bfb0b81f56f04520e
wavival <anaj0111719@gmail.com>
2025-09-24 14:22:21 -0500

    All

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/projects/ubuntu.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fubuntu.mdx&astroContentModuleFlag=true")]]);
+		
